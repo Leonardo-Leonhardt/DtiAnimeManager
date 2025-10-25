5882c90b251bec6c174192d715bbd274cc170927
Leonardo Bispo <1453450@sga.pucminas.br>
2025-10-25 13:25:44 -0300

    Commit inicial do projeto Visual Studio

---
+++ Models/Anime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtiAnimeManager.Models
+{
+    internal class Anime
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string Genero { get; set; }
+        public string DataDeLancamento{get;set;}
+        public double? Nota { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DtiAnimeManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
